7f42584ac6d485f9847d5736ecfc5ab8eeddc5da
Luis Zelph Moreno Ruiz <luiszelph.morenoruiz@gmail.com>
2024-05-01 16:19:45 -0600

    Se agregaron los botones del menu egresos e ingresos

--- src/app/components/egresos/egresos.component.ts
+++ src/app/components/egresos/egresos.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-egresos',
   templateUrl: './egresos.component.html',
-  styleUrls: ['./egresos.component.css']
+  styleUrls: ['./egresos.component.scss']
 })
 export class EgresosComponent {
 
--- src/app/components/ingresos/ingresos.component.ts
+++ src/app/components/ingresos/ingresos.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-ingresos',
   templateUrl: './ingresos.component.html',
-  styleUrls: ['./ingresos.component.css']
+  styleUrls: ['./ingresos.component.scss']
 })
 export class IngresosComponent {
 
